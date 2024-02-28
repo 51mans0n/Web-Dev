80a06674f61e2d724a120c3b04493955b7c25d2f
Maxim Skorokhod <115411485+51mans0n@users.noreply.github.com>
2024-02-28 21:32:41 +0600

    Lab5 finished

---
+++ Lab5/products-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+export interface Category {
+  name: string;
+  products: Product[];
+}
+
+export interface Product {
+  image: string[];
+  name: string;
+  description: string;
+  rating: number;
+  link: string;
+}
